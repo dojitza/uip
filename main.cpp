d6c71024cbe5a466302c17cb04359740ca62d78d
Dominik Kotarski <dojitza@gmail.com>
2020-02-09 15:30:32 +0100

    WIP Setting up sdl2 on windows

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 #include "CommandLineRenderer.hpp"
 #include "Renderer.hpp"
 #include <vector>
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <SDL.h>
+#include <stdio.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[]) {
     Renderer *renderer = new CommandLineRenderer();
     std::vector<Point3d> points;
     for(int i=0; i<10; ++i){
@@ -11,6 +17,40 @@ int main() {
         renderer->drawLine(points[0], points[i]);
     }
 
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //Wait two seconds
+            SDL_Delay( 2000 );
+        }
+    }
 
     return 0;
 }
